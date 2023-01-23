99adc5b05bd14e9a947a692de764d9f25fce86db
aisyahbkl1245 <111877720+aisyahbkl1245@users.noreply.github.com>
2023-01-23 10:03:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6287898307350'],
-  ['6287898307350'],
-  ['6287898307350', 'Owner', true]
+  ['6289669152489'],
+  ['6285950091241'],
+  ['6282376267531', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
